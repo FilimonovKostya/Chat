4c1f892fc7ca29139181dfe888ad9312ff0c1f5d
FilimonovKostya <konstantinfilimonou@gmail.com>
2022-01-31 14:23:50 +0300

    add socket

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
 const express = require('express');
+const http = require('http')
 const app = express();
+const server = http.createServer(app)
+const {Server} = require('socket.io')
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        credentials: true
+    }
+})
 
-const port = 8888;
-
-const chanels = new Map([
- 
-])
+const rooms = new Map()
 
 app.get('/rooms', (req, res) => {
     console.log('Request on rooms')
-    res.json(chanels)
+    res.json(rooms)
 })
 
-app.listen(port, (err) => {
+io.on('connection', socket => {
+    console.log('socket connected')
+})
+
+server.listen('8080', (err) => {
     if(err){
         throw Error(err)
     }
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import {io} from 'socket.io-client'
 
+
+
+const connectSocket = () => {
+    io("http://localhost:8080")
+}
 
 function App() {
     return (
         <div className="App">
-            test
+            Hello React
+            <button onClick={connectSocket}>Connect</button>
         </div>
     );
 }
