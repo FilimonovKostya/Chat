7aa53f6b8a05ba7e52a6d3f3464d86713bd9365b
FilimonovKostya <konstantinfilimonou@gmail.com>
2022-05-16 17:41:02 +0400

    was added fonts and have been started sign in page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import SignIn from "./components/SignIn";
+import './index.scss'
 
 function App() {
     return (
-        <>
+        <div className={'container'}>
             <SignIn/>
-        </>
+        </div>
     );
 }
 
