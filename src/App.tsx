dbd118a9e0523268a4b4adb4c517bc3f24ffa618
FilimonovKostya <konstantinfilimonou@gmail.com>
2022-05-16 13:37:07 +0400

    was removed dependecies and clean up components

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
-import React, {useReducer} from 'react';
-import JoinBlock from "./components/JoinBlock";
-import reducer from "./reducer";
-import socket from './socket'
+import React from 'react';
+
 
 function App() {
-    const [state, dispatch] = useReducer(reducer, {joined: false, roomId: null, userName: null});
-
-    console.log('State', state)
-
-    const onLogin = (object: Object) => {
-        dispatch({type: 'JOINED', payload: object})
-
-        socket.emit('ROOM:JOIN', object)
-    }
 
     return (
         <div className="wrapper">
-            {!state.joined && <JoinBlock onLogin={onLogin}/>}
+            hi
         </div>
     );
 }
