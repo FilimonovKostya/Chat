fb85b9d33f5d7d662e6a9385dc60e1c496da7c95
FilimonovKostya <konstantinfilimonou@gmail.com>
2022-05-17 17:20:03 +0400

    sign in , turn on material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
-import SignIn from "./components/SignIn";
-import './index.scss'
+import {Avatar, Box, Button, Checkbox, Container, FormControlLabel, TextField, Typography} from "@mui/material";
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 
 function App() {
     return (
-        <div className={'wrapper'}>
-            <div className={'container'}>
-                <SignIn/>
-            </div>
-        </div>
+        <Container maxWidth={'xs'} sx={{mt: 15}}>
+            <Box sx={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+                <Avatar sx={{background: '#9c27b0'}}> <LockOutlinedIcon/> </Avatar>
+
+                <Box component={"form"}>
+                    <Typography component={'h1'} variant={'h5'}>Sign in</Typography>
+                    <TextField margin={'normal'} color={'secondary'} label="Email Address" fullWidth variant="outlined" required/>
+                    <TextField margin={'normal'} color={'secondary'} label="Password" fullWidth variant="outlined" type={'password'} required/>
+                    <FormControlLabel control={<Checkbox defaultChecked color="secondary"/>} label={'Remember me'}/>
+                    <Button variant="contained" type={'submit'} sx={{mt: 1}} fullWidth>Sign in</Button>
+                </Box>
+
+            </Box>
+        </Container>
     );
 }
 
