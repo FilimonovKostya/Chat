8d0ef5e65e8dc720b105652390933d11e2681892
FilimonovKostya <konstantinfilimonou@gmail.com>
2022-05-18 12:48:07 +0400

    sign in page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Avatar, Box, Button, Checkbox, Container, FormControlLabel, TextField, Typography} from "@mui/material";
+import {Avatar, Box, Button, Checkbox, Container, FormControlLabel, Grid, Link, TextField, Typography} from "@mui/material";
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 
 function App() {
@@ -14,6 +14,20 @@ function App() {
                     <TextField margin={'normal'} color={'secondary'} label="Password" fullWidth variant="outlined" type={'password'} required/>
                     <FormControlLabel control={<Checkbox defaultChecked color="secondary"/>} label={'Remember me'}/>
                     <Button variant="contained" type={'submit'} sx={{mt: 1}} fullWidth>Sign in</Button>
+
+                    <Grid container marginTop={2} marginBottom={8}>
+                        <Grid item xs>
+                            <Link variant={'body2'} href={'#'}>Forgot password ?</Link>
+                        </Grid>
+                        <Grid item>
+                            <Link variant={'body2'} href={'#'}>Don't have an account? Sign Up</Link>
+                        </Grid>
+                    </Grid>
+
+                    <Box textAlign={'center'} color={'text.secondary'}>
+                        <Typography variant={'body2'}> Copyright information :)</Typography>
+                    </Box>
+
                 </Box>
 
             </Box>
