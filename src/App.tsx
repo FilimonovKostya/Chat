b1b640142c5caa4b5266ba2dfd13af90b63d0674
FilimonovKostya <konstantinfilimonou@gmail.com>
2022-05-19 18:06:31 +0400

    created header of chat

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import SignIn from './components/SignIn'
 import LogIn from './components/LogIn'
+import ChatPage from './components/ChatPage'
 
 function App() {
   return (
     <>
       {/*<SignIn/>*/}
-      <LogIn />
+      {/*<LogIn />*/}
+      <ChatPage/>
     </>
   )
 }
